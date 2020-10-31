83918f99a706fe0b464199c787ba743a9104e794
Prasheel Soni <prasheelsoni11@gmail.com>
2020-10-31 23:52:30 +0530

    initial commit

---
+++ src/components/Ingredients/Search.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import Card from '../UI/Card';
+import './Search.css';
+
+const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?title=${enteredFilter}`;
+        fetch(
+          'http://localhost:3000/ingredients' + query
+        )
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
+
+  return (
+    <section className="search">
+      <Card>
+        <div className="search-input">
+          <label>Filter by Title</label>
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
+        </div>
+      </Card>
+    </section>
+  );
+});
+
+export default Search;
